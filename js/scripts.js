ef1574ad8f6e19c8eb0417d9f6e6b89e24cfb832
Anderson Moreton <anderson.moreton@gmail.com>
2024-07-19 01:59:56 +0100

    Editing all pages.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const imageField = document.getElementById("image-field");
+const imagePreview = document.getElementById("image-preview");
+
+const loadImage = (e) => {
+
+    const filePath = e.target || window.e.srcElement;
+
+    const file = filePath.files;
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+        imagePreview.src = fileReader.result;
+    }
+
+    fileReader.readAsDataURL(file[0]);
+
+};
+
+imageField.addEventListener("change", loadImage);
