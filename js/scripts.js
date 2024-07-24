07de2bde141586941693bb0b56b8a77a2eb607ef
Anderson Moreton <anderson.moreton@gmail.com>
2024-07-24 21:26:08 +0100

    Editing page of the message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-// const imageField = document.getElementById("image-field");
-// const imagePreview = document.getElementById("image-preview");
+const imageField = document.getElementById("image-field");
+const imagePreview = document.getElementById("image-preview");
 
-// const loadImage = (e) => {
+const loadImage = (e) => {
 
-//     const filePath = e.target || window.e.srcElement;
+    const filePath = e.target || window.e.srcElement;
 
-//     const file = filePath.files;
+    const file = filePath.files;
 
-//     const fileReader = new FileReader();
+    const fileReader = new FileReader();
 
-//     fileReader.onload = () => {
-//         imagePreview.src = fileReader.result;
-//     }
+    fileReader.onload = () => {
+        imagePreview.src = fileReader.result;
+    }
 
-//     fileReader.readAsDataURL(file[0]);
+    fileReader.readAsDataURL(file[0]);
 
-// };
+};
 
-// imageField.addEventListener("change", loadImage);
+imageField.addEventListener("change", loadImage);
