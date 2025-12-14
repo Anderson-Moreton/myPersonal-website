ff5a10363760729ed1e484d23cc2cabe2011b43f
Anderson Moreton <anderson.moreton@gmail.com>
2025-12-14 23:49:55 +0000

    Update javascript navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,23 @@
-$(".open-btn").on("click", function () {
-    $(".sidebar").addClass("active");
+// $(".open-btn").on("click", function () {
+//     $(".sidebar").addClass("active");
+// });
+
+// $(".close-btn").on("click", function () {
+//     $(".sidebar").removeClass("active");
+// });
+
+"Use Strict";
+
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+// Show sidebar when open button is clicked
+openBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
 });
 
-$(".close-btn").on("click", function () {
-    $(".sidebar").removeClass("active");
+//Hide sidebar when a close button is clicked
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("active");
 });
-
